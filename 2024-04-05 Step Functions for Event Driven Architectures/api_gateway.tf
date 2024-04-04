21df97b01e30357295db54316ffbbd05511f0de0
Marcus Legendre <marcus.legendre@codecentric.de>
2024-04-04 18:07:34 +0200

    2024-04-05 WIP

--- 2024-04-05 Step Functions for Event Driven Architectures/api_gateway.tf
+++ 2024-04-05 Step Functions for Event Driven Architectures/api_gateway.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "step_function_role" {
   name = "${local.workshop_prefix}-stepFunctionExecutionRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
@@ -19,12 +19,12 @@ resource "aws_iam_role" "step_function_role" {
 resource "aws_iam_role_policy" "api_invoke_policy" {
   name   = "APIInvokeStepFunction"
   role   = aws_iam_role.step_function_role.id
-  policy = data.aws_iam_policy_document.api_invoke_policy_doc.json
+  policy = data.aws_iam_policy_document.step_function_integration.json
 }
 
 data "aws_iam_policy_document" "api_invoke_policy_doc" {
   statement {
-    actions   = ["states:StartExecution", "states:StartSyncExecution"]
+    actions = ["states:StartExecution", "states:StartSyncExecution"]
     resources = ["*"] # Specify Step Function ARN for better security
 
     effect = "Allow"
@@ -34,6 +34,8 @@ data "aws_iam_policy_document" "api_invoke_policy_doc" {
 resource "aws_sfn_state_machine" "sample_state_machine" {
   name     = "${local.workshop_prefix}-SampleStateMachine"
   role_arn = aws_iam_role.step_function_role.arn
+
+  type = "EXPRESS"
 
   definition = <<EOF
 {
@@ -55,10 +57,39 @@ resource "aws_apigatewayv2_api" "example_api" {
   description   = "Example API for Step Function integration"
 }
 
+data "aws_iam_policy_document" "step_function_integration_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "step_function_integration" {
+  statement {
+    actions = ["states:StartExecution", "states:StartSyncExecution"]
+
+    resources = [aws_sfn_state_machine.sample_state_machine.arn]
+  }
+}
+
+resource "aws_iam_role" "step_function_integration" {
+  name = "${local.workshop_prefix}-stepFunctionIntegrationRole"
+
+  assume_role_policy = data.aws_iam_policy_document.step_function_integration_assume.json
+  inline_policy {
+    name   = "InvokeStepFunction"
+    policy = data.aws_iam_policy_document.step_function_integration.json
+  }
+}
+
 resource "aws_apigatewayv2_integration" "step_function_integration" {
-  api_id           = aws_apigatewayv2_api.example_api.id
-  credentials_arn  = aws_iam_role.step_function_role.arn
-  integration_type = "AWS_PROXY"
+  api_id              = aws_apigatewayv2_api.example_api.id
+  credentials_arn     = aws_iam_role.step_function_integration.arn
+  integration_type    = "AWS_PROXY"
   integration_subtype = "StepFunctions-StartSyncExecution"
   request_parameters = {
     StateMachineArn = aws_sfn_state_machine.sample_state_machine.arn
