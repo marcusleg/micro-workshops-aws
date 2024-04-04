2a0a916db9f556b0361967f853ec5005b9d4d3ee
Marcus Legendre <marcus.legendre@codecentric.de>
2024-04-04 15:01:37 +0200

    2024-04-05 WIP

--- 2024-04-05 Step Functions for Event Driven Architectures/api_gateway.tf
+++ 2024-04-05 Step Functions for Event Driven Architectures/api_gateway.tf
@@ -16,7 +16,6 @@ resource "aws_iam_role" "step_function_role" {
   })
 }
 
-# IAM Policy to allow Step Function to be called by API Gateway
 resource "aws_iam_role_policy" "api_invoke_policy" {
   name   = "APIInvokeStepFunction"
   role   = aws_iam_role.step_function_role.id
@@ -25,14 +24,13 @@ resource "aws_iam_role_policy" "api_invoke_policy" {
 
 data "aws_iam_policy_document" "api_invoke_policy_doc" {
   statement {
-    actions   = ["states:StartExecution"]
-    resources = ["*"] # Specify your Step Function ARN for better security
+    actions   = ["states:StartExecution", "states:StartSyncExecution"]
+    resources = ["*"] # Specify Step Function ARN for better security
 
     effect = "Allow"
   }
 }
 
-# Step Function State Machine
 resource "aws_sfn_state_machine" "sample_state_machine" {
   name     = "${local.workshop_prefix}-SampleStateMachine"
   role_arn = aws_iam_role.step_function_role.arn
@@ -51,14 +49,12 @@ resource "aws_sfn_state_machine" "sample_state_machine" {
 EOF
 }
 
-# API Gateway v2
 resource "aws_apigatewayv2_api" "example_api" {
   name          = "${local.workshop_prefix}-exampleAPI"
   protocol_type = "HTTP"
   description   = "Example API for Step Function integration"
 }
 
-# Integration between API Gateway and Step Function
 resource "aws_apigatewayv2_integration" "step_function_integration" {
   api_id           = aws_apigatewayv2_api.example_api.id
   credentials_arn  = aws_iam_role.step_function_role.arn
@@ -69,18 +65,18 @@ resource "aws_apigatewayv2_integration" "step_function_integration" {
   }
 }
 
-# Default route for the API Gateway
 resource "aws_apigatewayv2_route" "default_route" {
   api_id    = aws_apigatewayv2_api.example_api.id
   route_key = "POST /execute"
   target    = "integrations/${aws_apigatewayv2_integration.step_function_integration.id}"
 }
 
-# Deploy the API
-resource "aws_apigatewayv2_stage" "example_stage" {
+resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.example_api.id
-  name        = "exampleStage"
+  name        = "$default"
   auto_deploy = true
+
+
 }
 
 output "api_gateway_url" {
